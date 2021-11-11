628f4ea89931da376f5d6c5eacd770e3c5329244
hussainshipu0356@gmail.com <hussainshipu0356@gmail.com>
2021-11-11 23:50:28 +0600

    new

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,7 @@
     <meta data-n-head="ssr" name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico">
     
-    <title>Bird</title>         
+    <title>Upsetti Metadata</title>         
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
